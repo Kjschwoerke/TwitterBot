213e8d60dfb200c146bb0520afc84e1e002dc50a
kjschwoerke <kjschwoerke@yahoo.com>
2020-07-13 13:20:01 -0500

    creating function to post a comment when user follows me

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+console.log('The "Follow-Bot" is starting!')
+
 require('dotenv').config()
 const Twit = require('twit')
 
@@ -10,18 +12,34 @@ const T = new Twit({
     strictSSL: true,     // optional - requires SSL certificates to be valid.
 })
 
+//setting up a user stream
+let stream = T.stream('statuses/filter', { track: '@Kenneth56744636' })
 
-let tweet = {
-    status: 'Testing "Post" for twitter bot'
+//stream anytime a user follows me (additional options on the Twitter API streaming documentation)
+stream.on('follow', followed)
+
+//create followed function that runs when a twitter user follows me.
+function followed(eventMsg) {
+    console.log('follow event')
+    let screenName = eventMsg.source.screen_name
+        tweetIt('@' + screenName + ' Thank you for following me!')
 }
 
-T.post('statuses/update', tweet, tweeted)
-
-function tweeted(err, data, response) {
-    if (err) {
-        console.error(`###There was an error: ${err}`)
-    } else{
-        console.log(data)
+//function to post a new tweet to twitter.
+function tweetIt(txt) {
+    //Create an object 'tweet' containing a status to be posted to the twitter account.
+    let tweet = {
+        status: txt
     }
-    
+    //make the post request to the twitter API.
+    T.post('statuses/update', tweet, tweeted)
+    //function to console.log(success or error) when post is made.
+    function tweeted(err, data, response) {
+        if (err) {
+            console.error(`###There was an error: ${err}`)
+        } else{
+            console.log(tweet.status)
+        }
+        
+    }
 }
